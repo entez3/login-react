d977e2fb3f3db2fcf2dcf7ff959d4736126f6ad3
ent <m.entezarian2@gmail.com>
2023-05-27 12:00:40 +0330

    create pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,17 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function Login() {
   return (
-    <div className="App">
-      <div className="login">
+    <div className="login">
         <h1>Login</h1>
-        <form method="post" onSubmit={handlelogin}>
+        <form method="post">
             <input type="text" placeholder="Username" required="required" />
             <input type="password" placeholder="Password" required="required" />
             <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
         </form>
     </div>
-    </div>
-  );
+  )
 }
 
-export default App;
+export default Login
