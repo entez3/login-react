82a0ff6db990e8f2a9ed1a11677f2e65843bf02d
ent <m.entezarian2@gmail.com>
2023-06-05 19:04:51 +0330

    connect to server with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Login from './pages/Login'
+import { useAuthState } from './context/auth-context'
+import Dashboard from './pages/Dashboard'
 
 function App() {
+  const { token } = useAuthState()
   return (
-    <Login />
+    <>
+      {token ? <Dashboard /> : <Login />}
+    </>
   )
 
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
+const fetchToken = async (username, password) => {
+  return axios.post('http://localhost:3001/login',
+    {
+      username,
+      password
+    })
+}
 
 function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    fetchToken(username, password)
+      .then(re => console.log(re.data))
+  }
   return (
     <div className="login">
-        <h1>Login</h1>
-        <form method="post">
-            <input type="text" placeholder="Username" required="required" />
-            <input type="password" placeholder="Password" required="required" />
-            <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
-        </form>
+      <h1>Login</h1>
+      <form method="post" onSubmit={handleLogin}>
+
+        <input value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Username"
+          required="required" />
+
+        <input value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          required="required" />
+
+        <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
+      </form>
     </div>
   )
 }
